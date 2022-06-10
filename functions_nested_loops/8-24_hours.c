b95973b579b9d427df396eab4d354074977a40d5
Bills <tsevillano@pucp.pe>
2022-06-10 10:23:14 -0700

    print a digital watch script added

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print a digital watch
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+		_putchar('0' + (i / 10));
+		_putchar('0' + (i % 10));
+		_putchar(':');
+		_putchar('0' + (j / 10));
+		_putchar('0' + (j % 10));
+		_putchar('\n');
+		}
+	}
+}
