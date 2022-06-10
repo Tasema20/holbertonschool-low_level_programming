7fb256855e2637888ffc3727f59cb8ec763c8ee5
Bills <tsevillano@pucp.pe>
2022-06-10 10:07:19 -0700

    getting the absolute value script added

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: input integer
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+
+}
