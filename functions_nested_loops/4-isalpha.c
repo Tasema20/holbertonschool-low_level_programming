b9a921fecfe988870b88b85e5c60c6a0c04a041f
Bills <tsevillano@pucp.pe>
2022-06-10 09:53:29 -0700

    looking for alpha char

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic char
+ * @c: The parameter 
+ * Return: 1 if c is lower/uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
