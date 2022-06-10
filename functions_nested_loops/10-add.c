07b5949daa2dd0fbd95310c13534fff657262bdf
Bills <tsevillano@pucp.pe>
2022-06-10 11:38:55 -0700

    add two int script added

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds two integers 
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: sum of the two input integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
