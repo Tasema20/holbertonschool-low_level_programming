df16dee9177415d932f555e607164411febac475
Bills <tsevillano@pucp.pe>
2022-06-17 08:04:29 -0700

    typo fixed

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,20 +9,15 @@
 
 void print_array(int *a, int n)
 {
-	int i, count;
+	int i;
 
 	i = 0;
-	count = 1;
-	if (n < 0)
-		n = 0;
-	if (n > 0)
+	while (i < n)
 	{
-		while (a[i] != '\0' && count < n)
-		{
-			printf("%d, ", a[i]);
-			i++;
-			count++;
-		}
-		printf("%d\n", a[i]);
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		i++;
 	}
+	printf("\n");
 }
