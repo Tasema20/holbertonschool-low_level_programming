92f59cc49e49559c4bfc1e65df2f446649f0a24c
Bills <tsevillano@pucp.pe>
2022-06-17 13:15:22 -0700

    betty fixed

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *  * reverse_array -  reverses input array
- *   * @a: input array
- *    * @n: number of elements of the array
- *     *
- *      * Return: void
- *       */
+ * reverse_array -  reverses input array
+ * @a: input array
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
 void reverse_array(int *a, int n)
 {		
 	int i = 0;
