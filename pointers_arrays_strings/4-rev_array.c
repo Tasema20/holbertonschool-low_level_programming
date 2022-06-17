279f4f55fcb473bcd439e86809c82363096cccc7
Bills <tsevillano@pucp.pe>
2022-06-17 13:13:18 -0700

    array reverse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  * reverse_array -  reverses input array
+ *   * @a: input array
+ *    * @n: number of elements of the array
+ *     *
+ *      * Return: void
+ *       */
+void reverse_array(int *a, int n)
+{		
+	int i = 0;
+	int tmp1, tmp2;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp1 = a[i];
+		tmp2 = a[n - 1 - i];
+ 		a[n - 1 - i] = tmp1;
+                a[i] = tmp2;
+	}
+}
