1a6f9601091ab32c1dafb48326e0d4dadad39233
Bills <tsevillano@pucp.pe>
2022-06-17 16:12:01 -0700

    betty fix2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -5,23 +5,21 @@
  * Return: string array
  */
 
-char *leet(char *s)
+char *leet(char *a)
 {
-	int i;
+	int i = 0, j;
+	char lo[] = {'a', 'e', 'o', 't', 'l'};
+	char up[] = {'A', 'E', 'O', 'T', 'L'};
+	char le[] = {'4', '3', '0', '7', '1'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (a[i])
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (a[i] == lo[j] || a[i] == up[j])
+			a[i] = le[j];
+		}
+		i++;
 	}
-
-	return (s);
+	return (a);
 }
