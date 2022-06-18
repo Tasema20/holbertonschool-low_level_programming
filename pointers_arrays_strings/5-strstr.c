8d7a2915c0beb479d708b4a99ea5f580ba8d207c
Bills <tsevillano@pucp.pe>
2022-06-17 21:54:29 -0500

    find the first occurence

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,37 +1,41 @@
-#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 /**
- * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: string to match
- * Return: pointer to initial segment of haystack
+ * *_strstr - char *_strstr(char *haystack, char *needle)
+ * @haystack: input string where input subtring needle will be searched
+ * @needle: input substring that will be searched in haystack
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
+	int m = 0, n = 0, i, found = 0;
 
-	i = 0;
-	j = 0;
-	start = 0;
-
-	while (needle[nlen] != '\0')
+	while (needle[m])
+		m++;
+	while (haystack[n])
 	{
-		nlen++;
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		found = 0;
+		/* printf("%s %s\n", &haystack[n], &needle[0]);*/
+		for (i = 0; i < m; i++)
 		{
-			if (j == 0)
+			if (haystack[n + i] == needle[i])
 			{
-				start = i;
-			}
-			if (j == nlen - 1)
-			{
-				return (haystack + start);
+				found += 1;
+				/* printf("GAAA\n");*/
+				if (found == m)
+					break;
 			}
 		}
+		if (found == m)
+			break;
+		n++;
 	}
-	return (0);
+	if (found)
+		return (&haystack[n]);
+	else if (!needle[0])
+		return (&haystack[0]);
+	else
+		return (NULL);
 }
