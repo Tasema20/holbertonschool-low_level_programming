98ea684f65f0eb2999c6a2fee41b1321290f58b1
Bills <tsevillano@pucp.pe>
2022-06-23 06:40:22 -0500

    second test

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  it is just an empty code
+ *
+ * Description: program will do nothing
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	return (0);
+}
